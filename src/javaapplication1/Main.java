deba3993b83d2c8419e2c537ed54c25c65e8b2e9
sourabhvrm <33700537+sourabhvrm@users.noreply.github.com>
2018-06-01 00:49:59 +0530

    Add files via upload

---
+++ src/javaapplication1/Main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaapplication1;
+
+/**
+ *
+ * @author Saurabh
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for(int i=0;i<0;i++){
+            System.out.println(i);
+        }
+        System.out.println(2);
+    }
+
+}
